739cec9b3f8f93d1a3aff688b1fb37165f3f7f4a
henrycharlton <henrycharlton1@gmail.com>
2024-02-21 07:56:28 +0100

    global_linear.py is giving the correct output, just have to make it go into a fasta file

--- project2/global_linear.py
+++ project2/global_linear.py
@@ -18,40 +18,51 @@ def read_fasta(file):
         else:
             seqs[header] += line.strip()
     return seqs
+
 ########### define function to read the Phylip-like control file ####
 # this will read in the score matrix, gap cost, and alphabet
+# the file will have the following format:
+#   4  #gap cost
+#   A  0  5  2  5 #alphabet[0], scoring matrix[0,:]
+#   C  5  0  5  2 #alphabet[1], scoring matrix[1,:]
+#   G  2  5  0  5 #alphabet[2], scoring matrix[2,:]
+#   T  5  2  5  0 #alphabet[3], scoring matrix[3,:]
+
 def read_control_file(file):
     with open(file, 'r') as f:
         lines = f.readlines()
-    # get the alphabet
-    alphabet = lines[0].strip().split()
     # get the gap cost
-    gap_cost = int(lines[1].strip())
+    gap_cost = int(lines[0].strip())
+    # get the alphabet
+    alphabet = []
     # get the scoring matrix
-    scoring_matrix = np.array([list(map(int, line.strip().split())) for line in lines[2:]])
-    return alphabet, gap_cost, scoring_matrix
-
+    scoring_matrix = []
+    for line in lines[1:]:
+        line = line.strip().split()
+        alphabet.append(line[0])
+        scoring_matrix.append([int(x) for x in line[1:]])
+    return gap_cost, alphabet, np.array(scoring_matrix)
 
 ########### default scoring matrix ################################### 
 # we won't take this as an input 
-scoring_matrix = np.array([[2,0,0,0],
-                           [0,2,0,0],
-                           [0,0,2,0],
-                           [0,0,0,2]])
+# scoring_matrix = np.array([[2,0,0,0],
+#                            [0,2,0,0],
+#                            [0,0,2,0],
+#                            [0,0,0,2]])
 
 
 
 ######## Cost function between two nucleotides #######################
-def cost(nuc1, nuc2, scoring_matrix = scoring_matrix):
+def cost(nuc1, nuc2, scoring_matrix):
 #   will use this for indexing
-    nucleotides = ['A', 'C', 'G', 'T']
+    nucleotides = ['a','c','g','t']
 #   set the scoring matrix the scoring matrix
     scoring_matrix = scoring_matrix
 #   index into the scoring matrix based on the given nucleotides to get cost
     return scoring_matrix[nucleotides.index(nuc1), nucleotides.index(nuc2)]
 
 ######## define the optimality table function  #######################
-def optimal(A,B, scoring_matrix = scoring_matrix, gap_cost = 1):
+def optimal(A,B, scoring_matrix, gap_cost):
     # get table dimensions
     n = len(A)
     m = len(B)
@@ -65,14 +76,14 @@ def optimal(A,B, scoring_matrix = scoring_matrix, gap_cost = 1):
     # make it work for gapcost = 0
     if gap_cost != 0:
         # make a list of length n filled with 0, -gapcost, -2*gapcost, ... -n*gapcost
-        T[:,0] = [0 - gap_cost*i for i in range(n+1)]
+        T[:,0] = [0 + gap_cost*i for i in range(n+1)]
     else:
         T[:,0] = np.zeros(n+1)
 
     # set the 0th row of T to be 0, -gapcost, -2*gapcost, ... -n*gapcost
     # make it work for gapcost = 0
     if gap_cost != 0:
-        T[0,:] = [0 - gap_cost*i for i in range(m+1)]
+        T[0,:] = [0 + gap_cost*i for i in range(m+1)]
     else:
         T[0,:] = np.zeros(m+1)
 
@@ -82,15 +93,15 @@ def optimal(A,B, scoring_matrix = scoring_matrix, gap_cost = 1):
     # or a gap in B (left) defined by gap cost (-1)
     for i in range(1,n+1):
         for j in range(1,m+1):
-            T[i,j] = max(
+            T[i,j] = min(
                 T[i-1,j-1] + cost(A[i-1],B[j-1], scoring_matrix), 
-                T[i-1,j] - gap_cost, 
-                T[i,j-1] - gap_cost
+                T[i-1,j] + gap_cost, 
+                T[i,j-1] + gap_cost 
             )
     return T
 
 ######## define the backtracking function  #######################
-def backtrack_one(A,B, scoring_matrix = scoring_matrix, gap_cost = 1):
+def backtrack_one(A,B, scoring_matrix, gap_cost):
     align_A = ''
     align_B = ''
     i = len(A)
@@ -99,13 +110,13 @@ def backtrack_one(A,B, scoring_matrix = scoring_matrix, gap_cost = 1):
     # keep iterating while we haven't reached the end of either sequence
     while i > 0 and j > 0:
         # if the score in T came from a match/mismatch...
-        if T[i,j] == T[i-1,j-1] + cost(A[i-1],B[j-1]):
+        if T[i,j] == T[i-1,j-1] + cost(A[i-1],B[j-1], scoring_matrix):
             align_A = A[i-1] + align_A
             align_B = B[j-1] + align_B
             i -= 1
             j -= 1
         # if the score in T came from a gap in A...
-        elif T[i,j] == T[i-1,j] - gap_cost:
+        elif T[i,j] == T[i-1,j] + gap_cost:
             align_A = A[i-1] + align_A
             align_B = '-' + align_B
             i -= 1
@@ -114,25 +125,61 @@ def backtrack_one(A,B, scoring_matrix = scoring_matrix, gap_cost = 1):
             align_A = '-' + align_A
             align_B = B[j-1] + align_B
             j -= 1
-    return (align_A, align_B)
+    return (align_A, align_B, T)
 
-
-############ user input ########################################
-choice = input("Request alignment of sequences? (y/n): ")
 
 # main function
 def main():
     # get the sequences
-    seqs = read_fasta(sys.argv[1])
+    gap_cost, alphabet, scoring_matrix = read_control_file(sys.argv[1])
+    seqs = read_fasta(sys.argv[2])
     # get the sequences
     A = list(seqs.values())[0]
     B = list(seqs.values())[1]
     # get the alignment
-    alignment = backtrack_one(A,B)
+    alignment = backtrack_one(A,B, scoring_matrix = scoring_matrix, gap_cost = gap_cost)
+    # print the parameters
+    print('Gap cost: ', gap_cost)
+    print('Scoring matrix: \n', scoring_matrix)
+    print('sequences: \n', A, '\n', B)
     # print the alignment
-    if choice == 'y'| choice == 'Y':
-        #store the alignment in a file
-        with open('alignment.txt', 'w') as f:
-            f.write(alignment[0] + '\n' + alignment[1])
+    print('alignment: \n', alignment[0], '\n', alignment[1])
     # print the score
-    print(optimal(A,B)[-1,-1])
+    print(alignment[2])
+    
+
+# run the main function
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+# ############ user input ########################################
+# choice = input("Request alignment sequence alignment? (y/n): ")
+
+# # main function
+# def main():
+#     # get the sequences
+#     gap_cost, alphabet, scoring_matrix = read_control_file(sys.argv[1])
+#     seqs = read_fasta(sys.argv[2])
+#     # get the sequences
+#     A = list(seqs.values())[0]
+#     B = list(seqs.values())[1]
+#     # get the alignment
+#     alignment = backtrack_one(A,B, scoring_matrix = scoring_matrix, gap_cost = gap_cost)
+#     # print the alignment
+#     if (choice == 'y'| choice == 'Y'):
+#         #store the alignment in a file in the directory where the script is run
+#         with open('alignment.txt', 'w') as f:
+#             f.write(alignment[0] + '\n')
+#             f.write(alignment[1] + '\n')
+#     # print the score
+#     print(optimal(A,B)[-1,-1])
