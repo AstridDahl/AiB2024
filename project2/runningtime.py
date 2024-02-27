e56bdfa78f7097251ee3d632fd13b1740e0fc440
AstridDahl <88832493+AstridDahl@users.noreply.github.com>
2024-02-27 17:34:47 +0100

    lasttests

--- project2/runningtime.py
+++ project2/runningtime.py
@@ -36,51 +36,49 @@ import matplotlib.pyplot as plt
 #     now = timer()
 # theoretical_running_times = now - then
 
-
 # linear
 # time = user + sys
 # test1, 12: 0.142s
-
 # test4, 200: 0.202s
-
 # test5, 647: 0.570s
-
 # test6, 1781: 3.423s
-
+# test7, 4049: 18.352s
+# test8, 7289: 58.315s
+# test9, 15713: 288.376s
 
 # affine
 # time = user + sys
 # test1, 12: 0.144s
-
 # test4, 200: 0.416s
-
 # test5, 647: 0.528s
-
 # test6, 1781: 3.166s
+# test7, 4049: 15.933s
+# test8, 7289: 49.407s
+# test9, 15713: 257.434s
 
-ns = [12, 200, 647, 1781]
-lin_run = [0.142, 0.202, 0.57, 3.423]
-aff_run = [0.144, 0.416, 0.528, 3.166]
-lin_ys = [0.142/12**2, 0.202/200**2, 0.57/647**2, 3.423/1781**2]
-aff_ys = [0.144/12**3, 0.416/200**3, 0.528/647**3, 3.166/1781**3]
+ns = [12, 200, 647, 1781, 4049, 7289, 15713] # sequence length
+lin_run = [0.142, 0.202, 0.57, 3.423, 18.352, 58.315, 288.376] # empirical
+aff_run = [0.144, 0.416, 0.528, 3.166, 15.933, 49.407, 257.434] # empirical
+lin_ys = [0.142/12**2, 0.202/200**2, 0.57/647**2, 3.423/1781**2, 18.352/4049**2, 58.315/7289**2, 288.376/15713**2] # T(n) / n^2
+aff_ys = [0.144/12**3, 0.416/200**3, 0.528/647**3, 3.166/1781**3, 15.933/4049**3, 49.407/7289**3, 257.434/15713**3] # T(n) / n^3
 
 
-plt.plot(ns,lin_run)
-plt.xlabel("Input size")
-plt.ylabel("T(n)")
-plt.savefig("linT_plot.pdf")
+# plt.plot(ns,lin_run)
+# plt.xlabel("Input size")
+# plt.ylabel("T(n)")
+# plt.savefig("linT_plot.pdf")
 
-plt.plot(ns,aff_run)
-plt.xlabel("Input size")
-plt.ylabel("T(n)")
-plt.savefig("affT_plot.pdf")
+# plt.plot(ns,aff_run)
+# plt.xlabel("Input size")
+# plt.ylabel("T(n)")
+# plt.savefig("affT_plot.pdf")
 
-plt.plot(ns,lin_ys)
-plt.xlabel("Input size")
-plt.ylabel("T(n) / n^2")
-plt.savefig("lin_plot.pdf")
+# plt.plot(ns,lin_ys)
+# plt.xlabel("Input size")
+# plt.ylabel("T(n) / n^2")
+# plt.savefig("lin_plot.pdf")
 
-plt.plot(ns,lin_ys)
+plt.plot(ns, aff_ys)
 plt.xlabel("Input size")
 plt.ylabel("T(n) / n^3")
 plt.savefig("aff_plot.pdf")
