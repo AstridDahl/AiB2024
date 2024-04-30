bd0afee472e8edcc2db645d83bf77d6ef9a68362
henrycharlton <henrycharlton1@gmail.com>
2024-04-30 13:33:24 +0200

    compute first six newick trees

--- project5/henry/nj_fast.py
+++ project5/henry/nj_fast.py
@@ -1,4 +1,10 @@
 import numpy as np
+import time
+import sys
+from functools import lru_cache
+
+time1 = time.time()
+
 def read_control_file(file):
     with open(file, 'r') as f:
         lines = f.readlines()
@@ -24,33 +30,28 @@ class Node:
     def add_child(self, child):
         self.children.append(child)
     def remove_children(self, children):
-        for child in children:
-            self.children.remove(child)
+        self.children = [child for child in self.children if child not in children]
     def __repr__(self):
         return str(self.key)
     
 #### Helper functions #####
 # calculate_r: function to calculate the r value for a given node
+@lru_cache(maxsize=None)
 def calculate_r(i, D):
-    return D[i,:].sum() / (D.shape[0]-2)
+    return np.sum(D[i, :]) / (D.shape[0] - 2)
 
 # create_N: function to create the N matrix from the D matrix
 def create_N(D):
     N = np.zeros(D.shape)
     for i in range(D.shape[0]):
-        for j in range(D.shape[1]):
-            if i != j:
-                N[i,j] = (D[i,j] - calculate_r(i, D) - calculate_r(j, D))
+        N[i, :] = np.where(i != np.arange(D.shape[0]), D[i, :] - calculate_r(i, D) - calculate_r(np.arange(D.shape[0]), D), 0)
     return N
+
 # update_D: function to update the D matrix given the minimum indices which will be removed
 def update_D(D, min_idx):
-    # Get the indices that are not in min_idx
     not_min_idx = np.array([x for x in range(D.shape[0]) if x not in min_idx])
-    # Calculate the new distances
     new_distances = 0.5 * (D[min_idx[0], not_min_idx] + D[min_idx[1], not_min_idx] - D[min_idx[0], min_idx[1]])
-    # Create the new distance matrix by selecting the rows and columns not in min_idx
     D_new = D[np.ix_(not_min_idx, not_min_idx)]
-    # Add the new distances to the last row and column
     D_new = np.block([[D_new, new_distances.reshape(-1, 1)], [new_distances, 0]])
     return D_new
 
@@ -63,54 +64,36 @@ def tree_to_newick(node):
         
 ##### nj function ####
 def nj(path):
-    # read in the data
     gap_cost, S, D = read_control_file(path)
-
-    # initialize the tree with the taxa
     tree = Node('pseudo_root')
     for s in S:
         node = Node(s)
         node.parent = tree
         tree.add_child(node)
     while len(S) > 3:
-        # create the N matrix
         N = create_N(D)
-        # based on S, create the dictionaries to go back and forth between indices and taxa names
         stoi = {s:i for i,s in enumerate(S)}
         itos = {i:s for s,i in stoi.items()}
-        # get the index of the minimum value in the N matrix, excluding the diagonal
         min_idx = np.unravel_index(np.argmin(N + np.eye(N.shape[0])*np.max(N)), N.shape)
-        # taxa names of the minimum indices according to N
         min_taxa = [itos[x] for x in min_idx]
-        ### tree creation ###
-        # add a new node to the tree with the joined taxa
         new_node = Node(f"{min_taxa[0]}{min_taxa[1]}")
-        # define nodes for the min_taxa, which will be children of the joined node
-        # they could already be nodes with children
         child1 = [x for x in tree.children if x.key == min_taxa[0]][0]
         child2 = [x for x in tree.children if x.key == min_taxa[1]][0]
-        # assign the parent of the children
         child1.parent = new_node
         child2.parent = new_node
-        # assign their distances to the parent (joined node)
         child1.dist_to_parent = 0.5*(D[stoi[min_taxa[0]], stoi[min_taxa[1]]] \
                                     + calculate_r(stoi[min_taxa[0]], D) \
                                     - calculate_r(stoi[min_taxa[1]], D))
         child2.dist_to_parent = D[stoi[min_taxa[0]], stoi[min_taxa[1]]] \
                                 - child1.dist_to_parent
-        # add the children to the new node
         new_node.add_child(child1)
         new_node.add_child(child2)
-        # add the new node to the top level of the tree
         tree.add_child(new_node)
-        # remove the nodes corresponding to the joined taxa from the top level of the tree
         tree.remove_children([x for x in tree.children if x.key in min_taxa])
         D = update_D(D, min_idx)
-        # add the new node to the list of taxa
         S.append(new_node.key)
         S = [x for x in S if x not in min_taxa]
 
-    # termination: assign the distances between the last three nodes and the root
     stoi = {s:i for i,s in enumerate(S)}
     itos = {i:s for s,i in stoi.items()}
     tree.children[0].dist_to_parent = 0.5*(D[stoi[tree.children[0].key], stoi[tree.children[1].key]] \
@@ -124,9 +107,10 @@ def nj(path):
                                             - D[stoi[tree.children[0].key], stoi[tree.children[1].key]])
     return tree_to_newick(tree) + ';'
 
-# first argument is path to control file used to build tree
-import sys
 def main():
     print(nj(sys.argv[1]))
+
 if __name__ == "__main__":
     main()
+    time2 = time.time()
+    print(time2 - time1)
